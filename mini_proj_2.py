41c45b68a7d225112152ed6246e84453bacb8c48
Jessica Huynh <jhuynh@ualberta.ca>
2016-11-17 09:26:48 -0700

    added prompt for input db

--- mini_proj_2.py
+++ mini_proj_2.py
@@ -1,7 +1,24 @@
 # Mini Project 2
+import os.path
+import re
+
+def connectToInputDB():
+	dbFile = raw_input("Please enter an input database (name.db): ")
+
+	# check if file exits
+	if (not os.path.isfile(dbFile)) and (not re.match(".+\\.db", dbFile)):
+		print "Invalid filename, please try again!"
+		connectToInputDB()
+
+	print "Its a valid file..."
+
 
 def main():
 	print "Cmput 291: Mini Project 2"
+
+	connectToInputDB()
+
+
 
 if __name__ == '__main__':
 	main()
