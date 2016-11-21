79511142962f49102e41440ddd0c9d6b4469f4a0
Jessica Huynh <jhuynh@ualberta.ca>
2016-11-20 20:11:34 -0700

    trying to get minimal cover QQ

--- schema3nf.py
+++ schema3nf.py
@@ -10,7 +10,7 @@ def getFDTableName(conn, c):
 
 	return result[0]
 
-def getFDDictionary(conn, c, tableName):
+def getFDSet(conn, c, tableName):
 	query = 'SELECT * FROM ' + tableName + ';'
 	c.execute(query)
 	results = c.fetchall()
@@ -19,25 +19,55 @@ def getFDDictionary(conn, c, tableName):
 		print "Error: unable to get data from table " + tableName
 		exit()
 
-	fdDict = {'LHS' : 'RHS'} #dummy to start the table
+	fdSet = set()
 	for r in results:
-		fdDict[r['LHS']] = r['RHS']
+		fdSet.add((r['LHS'], r['RHS']))
 
-	# for key in fdDict: # keys = LHS
-	# 	print key + " --> " + fdDict[key]
+	# # ----------------------- TESTING -----------------------
+	# for fd in fdSet: # keys = LHS
+	# 	print fd
 
-	return fdDict
+	return fdSet
+
+def getClosure(lhs, fdSet):
+	return ''
 
 def computeMinimalCover(conn, c):
 	print "Computing minimal cover"
 
 	# select the FDs table
 	fdTableName = getFDTableName(conn, c)
-	fdDict = getFDDictionary(conn, c, fdTableName)
-	
-	for key in fdDict: # keys = LHS
-		print key + " --> " + fdDict[key]
+	fdSet = getFDSet(conn, c, fdTableName)
 
 	# 1. make the RHS of each FD into a single attribute
+	tempSet = set()
+	for fd in fdSet:
+		if "," in fd[1]:
+			rhsList = fd[1].split(',')
+			for c in rhsList:
+				tempSet.add((fd[0],c))
+		else:
+			tempSet.add(fd)
+	fdSet = tempSet
+
 	# 2. Elimate redundant attribute from LHS
+	tempSet = set()
+	for fd in fdSet:
+		if ',' in fd[0]: # possible redundancy
+			lhsList = fd[0].split(',')
+			for c in lhsList:
+				#eliminate from lhs
+				tempLHS = lhsList.remove(c)
+				lhs = ''.join(tempLHS)
+
+				rhsClosure = getClosure(lhs, fdSet)
+				# check if we can get a closure that gets us the rhs
+
+		else:
+			tempSet.add(fd)
+	fdSet = tempSet
+
 	# 3. Delete redundant FDs
+
+
+
