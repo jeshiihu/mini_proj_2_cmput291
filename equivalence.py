45de68132bd6832b0800a981ac9a14906d1be769
kchin28 <kgchin@ualberta.ca>
2016-11-23 10:32:03 -0700

    changed import statements

---
+++ equivalence.py
@@ -0,0 +1,31 @@
+from schema3nf import *
+from helpers import *
+
+def checkEquivalence(F,G):
+	equiv = True
+	for f in F:
+		ac=getClosure(f[0],G)
+		ac = getStringSet(ac)
+		containsRHS = True
+		for i in f[1]:
+			if i not in ac:
+				containsRHS = False
+				break		
+
+		if not containsRHS:
+			equiv = False
+			break
+
+	return equiv
+
+
+# def main():
+# 	F = set([('A','C'),('A,C','D'),('E,A','D'),('E','F')])
+# 	G = set([('A','C,D'),('E','A,D,F')])
+
+# 	test = checkEquivalence(F,G)
+# 	print(test)
+
+
+
+# main()
